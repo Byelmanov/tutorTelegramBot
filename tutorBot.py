511c7c3b9aa39dd19a4c62cc9c9586b1a7a6103a
Byelmanov <yelmanovbohdan@gmail.com>
2020-03-13 18:47:37 +0200

    test bot

--- tutorBot.py
+++ tutorBot.py
@@ -1,11 +1,20 @@
 # pip install pyTelegramBotAPI
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot("928244332:AAFXeSpQSVauw_3Efi6P_oiLkdcxjz7QK-Y")
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Hello, you wrote me /start')
+
+@bot.message_handler(content_types=['text'])
+def text_message(message):
+
+    if message.text == "1":
+        bot.send_message(message.chat.id, 'You chose 1')
+    elif message.text == "2":
+        bot.send_message(message.chat.id, 'You chose 2')
 
 
 bot.polling()
