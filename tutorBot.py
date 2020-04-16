adf02048d003f0e2508feeddddebebe8b0cec292
Byelmanov <yelmanovbohdan@gmail.com>
2020-04-16 22:37:21 +0300

    check connection with DB, check if SELECT gives empty set

--- tutorBot.py
+++ tutorBot.py
@@ -3,6 +3,7 @@
 import telebot
 from telebot import types
 import pymysql
+from datetime import datetime
 
 bot = telebot.TeleBot("928244332:AAFXeSpQSVauw_3Efi6P_oiLkdcxjz7QK-Y")
 
@@ -20,13 +21,29 @@ try:
     data_obj = DataCurrSession()
 
 
-    def check_is_tutor_and_return_id_of_they(username):
-        cursor.execute('SELECT * FROM tutors_tg')
-        usernames_from_DB = cursor.fetchall()
+    def wrap_cursor_execute(sql):
+        if not DB.open:
+            global DB
+            DB = pymysql.connect('localhost', 'root', '', 'unnamed')
 
-        for i in usernames_from_DB:
-            if(i[1] == username):
-                return i[0]
+        row_count = cursor.execute(sql)
+        if (row_count == 0):
+            return None
+        else:
+            result = cursor.fetchall()
+            return result
+
+
+
+    def check_is_tutor_and_return_id_of_they(username):
+        usernames_from_DB = wrap_cursor_execute('SELECT * FROM tutors_tg')
+
+        if usernames_from_DB == None:
+            return False
+        else:
+            for i in usernames_from_DB:
+                if(i[1] == username):
+                    return i[0]
 
         return False
 
@@ -60,16 +77,18 @@ try:
                 WHERE tutors_tg.user_id = {};
             """.format(tutor_id)
 
-            cursor.execute(sql_to_get_groups)
-            array_of_groups = cursor.fetchall()
+            array_of_groups = wrap_cursor_execute(sql_to_get_groups)
 
-            keyboard_groups = types.ReplyKeyboardMarkup(row_width=3, one_time_keyboard=True)
-            for group in array_of_groups:
-                group_button = types.KeyboardButton(group[1])
-                keyboard_groups.add(group_button)
+            if (array_of_groups == None):
+                bot.send_message(message.chat.id, 'У вас нет групп')
+            else:
+                keyboard_groups = types.ReplyKeyboardMarkup(row_width=3, one_time_keyboard=True)
+                for group in array_of_groups:
+                    group_button = types.KeyboardButton(group[1])
+                    keyboard_groups.add(group_button)
 
-            msg = bot.send_message(message.chat.id, 'Выберите группу:', reply_markup=keyboard_groups)
-            bot.register_next_step_handler(msg, handle_group)
+                msg = bot.send_message(message.chat.id, 'Выберите группу:', reply_markup=keyboard_groups)
+                bot.register_next_step_handler(msg, handle_group)
 
         else:
             bot.send_message(message.chat.id, "Извините, но вы не преподаватель. У вас нет прав"
@@ -85,27 +104,29 @@ try:
         WHERE title = '{}';
         """.format(message.text)
 
-        cursor.execute(sql_to_set_group_id)
+        group_id = wrap_cursor_execute(sql_to_set_group_id)
 
-        group_id = cursor.fetchone()
-        data_obj.group_id = group_id[0]
+        # test it, I'm not sure that it must work - [0][0]
+        data_obj.group_id = group_id[0][0]
 
         sql_to_get_subjects = """
         SELECT title, type FROM subjects
         INNER JOIN group_subject ON subjects.id = group_subject.subject_id
         WHERE group_subject.group_id = {} AND group_subject.tutor_id = {};
         """.format(group_id[0], data_obj.tutor_id)
-        cursor.execute(sql_to_get_subjects)
 
-        array_of_subjects = cursor.fetchall()
+        array_of_subjects = wrap_cursor_execute(sql_to_get_subjects)
 
-        keyboard_subjects = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
-        for subject in array_of_subjects:
-            subject_button = types.KeyboardButton('{} ({})'.format(subject[0], subject[1]))
-            keyboard_subjects.add(subject_button)
+        if (array_of_subjects == None):
+            bot.send_message(message.chat.id, 'У этой группы пока нет предметов')
+        else:
+            keyboard_subjects = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
+            for subject in array_of_subjects:
+                subject_button = types.KeyboardButton('{} ({})'.format(subject[0], subject[1]))
+                keyboard_subjects.add(subject_button)
 
-        msg = bot.send_message(message.chat.id, 'Выберите предмет:', reply_markup=keyboard_subjects)
-        bot.register_next_step_handler(msg, handle_subject)
+            msg = bot.send_message(message.chat.id, 'Выберите предмет:', reply_markup=keyboard_subjects)
+            bot.register_next_step_handler(msg, handle_subject)
 
 
     def handle_subject(message):
@@ -113,55 +134,67 @@ try:
         SELECT id FROM subjects
         WHERE CONCAT(title, ' (', type, ')') = '{}';
         """.format(message.text)
-        cursor.execute(sql_to_get_subject_id)
-        subject_id = cursor.fetchone()
-        data_obj.subject_id = subject_id[0]
+
+        subject_id = wrap_cursor_execute(sql_to_get_subject_id)
+
+        data_obj.subject_id = subject_id[0][0]
 
         sql_to_get_students = """
         SELECT id, lastname, firstname FROM students
         WHERE group_id = {};
         """.format(data_obj.group_id)
-        cursor.execute(sql_to_get_students)
-        array_of_students = cursor.fetchall()
+
+        array_of_students = wrap_cursor_execute(sql_to_get_students)
+
+        if (array_of_students == None):
+            bot.send_message(message.chat.id, 'В этой группе пока нет студентов')
+        else:
+            def launch_students(message, i):
+
+                def handle_one_student(message, i, length):
+
+                    answer = True if (message.text == 'Есть') else False
+
+                    current_data_string = datetime.now().strftime("%Y:%m:%d %H:%M:%S")
+
+                    sql_to_insert_to_journal = """
+                    INSERT INTO journals (student_id, subject_id, value, created_at, updated_at)
+                    VALUES ({}, {}, {}, '{}', '{}')
+                    """.format(data_obj.curr_student_id, data_obj.subject_id, answer,
+                               current_data_string, current_data_string)
+
+                    try:
+                        if not DB.open():
+                            global DB
+                            DB = pymysql.connect('localhost', 'root', '', 'unnamed')
+
+                        cursor.execute(sql_to_insert_to_journal)
+                        DB.commit()
+                    except:
+                        DB.rollback()
+
+                    if(i < length - 1):
+                        i += 1
+                        launch_students(message, i)
 
 
-        def launch_students(message, i):
+                length_of_array_students = len(array_of_students)
 
-            def handle_one_student(message, i, length):
+                one_time_keyboard = False if (i < length_of_array_students - 1) else True
 
-                answer = True if (message.text == 'Есть') else False
+                keyboard_student = types.ReplyKeyboardMarkup(row_width=2,
+                                                             one_time_keyboard=one_time_keyboard)
+                btn_is = types.KeyboardButton('Есть')
+                btn_absent = types.KeyboardButton('Нет')
+                keyboard_student.row(btn_is, btn_absent)
+                msg = bot.send_message(message.chat.id,
+                                       array_of_students[i][1]+' '+array_of_students[i][2],
+                                       reply_markup=keyboard_student)
+                data_obj.curr_student_id = array_of_students[i][0]
 
-                # TODO inserting correct current time
-                sql_to_insert_to_journal = """
-                INSERT INTO journals (student_id, subject_id, value, created_at, updated_at)
-                VALUES ({}, {}, {}, '2020-04-16 00:00:00', '2020-04-16 00:00:00')
-                """.format(data_obj.curr_student_id, data_obj.subject_id, answer)
+                bot.register_next_step_handler(msg, handle_one_student, i, length_of_array_students)
 
-                try:
-                    cursor.execute(sql_to_insert_to_journal)
-                    DB.commit()
-                except:
-                    DB.rollback()
-
-                if(i < length - 1):
-                    i += 1
-                    launch_students(message, i)
-
-
-            length_of_array_students = len(array_of_students)
-
-            one_time_keyboard = False if (i < length_of_array_students - 1) else True
-
-            keyboard_student = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=one_time_keyboard)
-            btn_is = types.KeyboardButton('Есть')
-            btn_absent = types.KeyboardButton('Нет')
-            keyboard_student.row(btn_is, btn_absent)
-            msg = bot.send_message(message.chat.id, array_of_students[i][1]+' '+array_of_students[i][2], reply_markup=keyboard_student)
-            data_obj.curr_student_id = array_of_students[i][0]
-
-            bot.register_next_step_handler(msg, handle_one_student, i, length_of_array_students)
-
-        launch_students(message, 0)
+            launch_students(message, 0)
 
 except:
     print('Error')
