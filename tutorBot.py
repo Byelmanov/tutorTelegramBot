81e6e321e3d80fbd1c6ab4d231174df7a0e715bc
Byelmanov <yelmanovbohdan@gmail.com>
2020-04-30 11:44:36 +0300

    add comments

--- tutorBot.py
+++ tutorBot.py
@@ -1,28 +1,46 @@
 # pip install pyTelegramBotAPI
 # pip install pymysql
+
+# main library for bot
 import telebot
+# it's a keyboard
 from telebot import types
+# library for DB connection
 import pymysql
 from datetime import datetime
-
+# to show description of errors in except block
 import traceback
 
+# create a bot using our token
 bot = telebot.TeleBot("928244332:AAFXeSpQSVauw_3Efi6P_oiLkdcxjz7QK-Y")
+# connect to DB
 DB = pymysql.connect('localhost', 'root', '', 'unnamed')
 
+# all code in try-except because if error we need bot to continue running
 try:
+    # cursor to execute sql queries
     cursor = DB.cursor()
 
+    # to store information about current user instead of using global variables
     class DataCurrSession:
         group_id = 0
         subject_id = 0
         tutor_id = 0
         curr_student_id = 0
 
+    # object of this class
     data_obj = DataCurrSession()
 
 
     def wrap_cursor_execute(sql):
+        """
+        wrap function to check if DB connection is open
+        if open: okay, execute sql query
+        else : open and execute query
+        :param sql:
+        :return: DB collection with data if data
+                                        else None
+        """
         global DB
         if not DB.open:
             DB = pymysql.connect('localhost', 'root', '', 'unnamed')
@@ -37,6 +55,12 @@ try:
 
 
     def check_is_tutor_and_return_id_of_they(username):
+        """
+        when user write /start command this function checks if they are tutor or not
+        :param username: current username who wrote /start
+        :return: false if current user not tutor
+                tutor telegram login if tutor exist
+        """
         usernames_from_DB = wrap_cursor_execute('SELECT * FROM tutors_tg')
 
         if usernames_from_DB == None:
@@ -49,15 +73,18 @@ try:
         return False
 
 
+    # handler for /help command
     @bot.message_handler(commands=['help'])
     def help_handler(message):
+        # first name of current user
         user_name = message.from_user.first_name
         help_message = 'Рады преветствовать вас, {}!\nДля того чтобы начать, пожалуйста,' \
                        ' введите команду /start'.format(user_name)
 
+        # send message to user who wrote /help command
         bot.send_message(message.chat.id, help_message)
 
-
+    # handler for /start command
     @bot.message_handler(commands=['start'])
     def start_handler(message):
         username = message.from_user.username
@@ -66,8 +93,10 @@ try:
         tutor_id = check_is_tutor_and_return_id_of_they(username)
 
         if(tutor_id):
+            # if current user is tutor send Welcome message
             bot.send_message(message.chat.id, "Добро пожаловать, {}".format(user_name))
 
+            # store tutor id in object of DataCurrSession class
             data_obj.tutor_id = tutor_id
 
             sql_to_get_groups = """
@@ -78,17 +107,26 @@ try:
                 WHERE tutors_tg.user_id = {};
             """.format(tutor_id)
 
+            # get array of groups of current tutor
             array_of_groups = wrap_cursor_execute(sql_to_get_groups)
 
+            # if tutor has no group send message and DO NOT CONTINUE sending messages to that tutor-
+            # only waiting for /help or /start command
             if (array_of_groups == None):
                 bot.send_message(message.chat.id, 'У вас нет групп')
             else:
+                # if tutor has groups - create a keyboard with group.name as button
                 keyboard_groups = types.ReplyKeyboardMarkup(row_width=3, one_time_keyboard=True)
                 for group in array_of_groups:
                     group_button = types.KeyboardButton(group[1])
                     keyboard_groups.add(group_button)
 
+                # send message "Выберите группу" and show keyboard with groups
+                # reply_markup argument - means that user can only write to bot using out keyboard with groups
                 msg = bot.send_message(message.chat.id, 'Выберите группу:', reply_markup=keyboard_groups)
+                # msg - our message ('Выберите группу:')
+                # register_next_step_handler - it means that out message above will handle another handler
+                # function handle_group will handle group after clicking on keyboard
                 bot.register_next_step_handler(msg, handle_group)
 
         else:
@@ -105,11 +143,10 @@ try:
         WHERE title = '{}';
         """.format(message.text)
 
+        # get group id to store it in data_obj
         group_id = wrap_cursor_execute(sql_to_set_group_id)
 
-        # test it, I'm not sure that it must work - [0][0]
         data_obj.group_id = group_id[0][0]
-
 
         sql_to_get_subjects = """
         SELECT title, type FROM subjects
@@ -119,15 +156,19 @@ try:
 
         array_of_subjects = wrap_cursor_execute(sql_to_get_subjects)
 
+        # if that group has no subjects DO NOT CONTINUE processing, only send message
         if (array_of_subjects == None):
             bot.send_message(message.chat.id, 'У этой группы пока нет предметов')
         else:
+            # everything is like in previous handler
             keyboard_subjects = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
             for subject in array_of_subjects:
                 subject_button = types.KeyboardButton('{} ({})'.format(subject[0], subject[1]))
                 keyboard_subjects.add(subject_button)
 
             msg = bot.send_message(message.chat.id, 'Выберите предмет:', reply_markup=keyboard_subjects)
+
+            # handle_subject will handle that message (msg)
             bot.register_next_step_handler(msg, handle_subject)
 
 
@@ -137,6 +178,7 @@ try:
         WHERE CONCAT(title, ' (', type, ')') = '{}';
         """.format(message.text)
 
+        # get subject id to store it in data_obj
         subject_id = wrap_cursor_execute(sql_to_get_subject_id)
 
         data_obj.subject_id = subject_id[0][0]
@@ -150,13 +192,31 @@ try:
 
         array_of_students = wrap_cursor_execute(sql_to_get_students)
 
+        # if that group has no student send message and DO NOT CONTINUE processing
         if (array_of_students == None):
             bot.send_message(message.chat.id, 'В этой группе пока нет студентов')
         else:
+            # function in function because I need to handle students as chain one after another one
+            # cycle doesn't work for it
+            # it's like recursion but with 2 functions)
             def launch_students(message, i):
+                """
+                Main function which works with counter i which we are using as index for array of students
+                :param message: form this param we take chat.id to send messages only for current user
+                :param i: counter and index for array of students
+                :return:
+                """
 
                 def handle_one_student(message, i, length):
+                    """
 
+                    :param message: message obj, we use it to get text of the message from user and to get chat.id
+                    :param i: counter, index for array
+                    :param length: length of array of students (if out of bounds) do not call launch_students()
+                    :return:
+                    """
+
+                    # convert answer 'Есть', "Нету" into bool to send in DB
                     answer = 1 if (message.text == 'Есть') else 0
 
                     current_data_string = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -167,19 +227,21 @@ try:
                     """.format(data_obj.curr_student_id, data_obj.subject_id, answer,
                                current_data_string, current_data_string)
 
-
-
+                    # try to write data into DB
                     try:
                         global DB
                         if not DB.open:
                             DB = pymysql.connect('localhost', 'root', '', 'unnamed')
 
                         cursor.execute(sql_to_insert_to_journal)
+                        # if okay - commit changes
                         DB.commit()
                     except Exception as e:
+                        # if error - rollback and print error in console
                         DB.rollback()
                         print(traceback.format_exc())
 
+                    # i < than length of array of student - continue
                     if(i < length - 1):
                         i += 1
                         launch_students(message, i)
@@ -187,26 +249,38 @@ try:
 
                 length_of_array_students = len(array_of_students)
 
+                # one_time_keyboard - is a param for keyboard. Here I check
+                # if current student is the last in array: hide keyboard after him
+                # else do not hide keyboard
                 one_time_keyboard = False if (i < length_of_array_students - 1) else True
 
+                # creating a keyboard
                 keyboard_student = types.ReplyKeyboardMarkup(row_width=2,
                                                              one_time_keyboard=one_time_keyboard)
+                # two buttons for keyboard
                 btn_is = types.KeyboardButton('Есть')
                 btn_absent = types.KeyboardButton('Нет')
                 keyboard_student.row(btn_is, btn_absent)
+                # msg with fullname of student
+                # array_of_students[i][1] - lastname array_of_students[i][2] - firstname
                 msg = bot.send_message(message.chat.id,
                                        array_of_students[i][1]+' '+array_of_students[i][2],
                                        reply_markup=keyboard_student)
+                # store id of current student in data_obj to get easy access to it in handle_one_student func
                 data_obj.curr_student_id = array_of_students[i][0]
 
+                # as handler for message (msg) we set handle_one_student function
+                # then handle_one_student will call current function and this 'recursion' will
+                # be continued while i < length of array of students
                 bot.register_next_step_handler(msg, handle_one_student, i, length_of_array_students)
 
             launch_students(message, 0)
 
 except:
     print('Error')
-
+    print(traceback.format_exc())
 finally:
+    # for bot working all time
     bot.polling()
     DB.close()
 
